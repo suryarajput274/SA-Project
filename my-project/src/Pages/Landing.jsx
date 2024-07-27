67ee04aa932e1b5d2dbe7d7c59f5c4a77b848426
AnkitChauhan39 <ankitchauhanbly88@gmail.com>
2024-07-27 11:10:13 +0530

    Services section added

--- my-project/src/Pages/Landing.jsx
+++ my-project/src/Pages/Landing.jsx
@@ -1,13 +1,19 @@
-import Navbar from '../component/Landingcomp/Navbar'
-import Hero from '../component/Landingcomp/Hero'
+import Navbar from "../component/Landingcomp/Navbar";
+import Hero from "../component/Landingcomp/Hero";
+import Services from "../component/Landingcomp/Services";
 
 const Landing = () => {
   return (
-    <div className='' > 
-        <Navbar/>
-        <Hero/>
+    <div className="">
+      <div className="fixed w-full top-0 z-10 bg-white">
+        <Navbar />
+      </div>
+      <div className="pt-16">
+        <Hero />
+        <Services />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
